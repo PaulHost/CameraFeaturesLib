7976a5c20ac74bdaa85b6e0047971dd1fc1d296e
Paul Hostev <paul.hostev@gmail.com>
2019-11-07 23:19:27 +0300

    add median blur

--- src/SlowMotion.cpp
+++ src/SlowMotion.cpp
@@ -1,8 +1,8 @@
 #include "SlowMotion.h"
 
-cv::Mat toGrayScale(cv::Mat frame) {
+cv::Mat getGrayScale(cv::Mat frame) {
     cv::Mat gray_frame;
-    cv::cvtColor(frame,gray_frame, cv::COLOR_BGR2GRAY);
+    cv::cvtColor(frame, gray_frame, cv::COLOR_BGR2GRAY);
     return gray_frame;
 }
 
@@ -12,22 +12,21 @@ cv::Mat calculateOpticalFlow(cv::Mat first, cv::Mat second) {
     return result;
 }
 
-void interpolateFrame(cv::Mat flow,cv::Mat previous_frame, cv::Mat& frame, double interpolation_value) {
-    cv::Mat result;
-    flow.forEach<cv::Point2f>([&](cv::Point2f& direction, const int position[]) {
+void interpolateFrame(cv::Mat flow, cv::Mat previous_frame, cv::Mat &frame, double interpolation_value) {
+    flow.forEach<cv::Point2f>([&](cv::Point2f &direction, const int position[]) {
         uchar x = std::round(position[0] + (direction.x * interpolation_value));
         uchar y = std::round(position[1] + (direction.y * interpolation_value));
-        frame.at<cv::Point3_<uint8_t>>(x, y) += previous_frame.at<cv::Point3_<uint8_t>>(position[0], position[1]) * interpolation_value;
+        frame.at<cv::Point3_<uint8_t >>(x, y) +=
+                previous_frame.at<cv::Point3_<uint8_t >>(position[0], position[1]) * interpolation_value;
     });
-
 }
 
 std::vector<cv::Mat> SlowMotion::interpolatedFrames(cv::Mat previous_frame, cv::Mat current_frame, uchar frame_count) {
-    cv::Mat previous_frame_gray = toGrayScale(previous_frame);
-    cv::Mat current_frame_gray = toGrayScale(current_frame);
+    cv::Mat previous_frame_gray = getGrayScale(previous_frame);
+    cv::Mat current_frame_gray = getGrayScale(current_frame);
 
-    cv::Mat forward_flow = calculateOpticalFlow(previous_frame, current_frame);
-    cv::Mat backward_flow = calculateOpticalFlow(current_frame, previous_frame);
+    cv::Mat forward_flow = calculateOpticalFlow(previous_frame_gray, current_frame_gray);
+    cv::Mat backward_flow = calculateOpticalFlow(current_frame_gray, previous_frame_gray);
 
     cv::Mat forward_flow_step = forward_flow / frame_count;
     cv::Mat backward_flow_step = backward_flow / frame_count;
@@ -36,11 +35,13 @@ std::vector<cv::Mat> SlowMotion::interpolatedFrames(cv::Mat previous_frame, cv::
     double interpolation_value = 1 / frame_count;
 
     for (uchar i = 1; i <= frame_count; i++) {
-        cv::Mat frame(previous_frame.rows, previous_frame.cols, CV_8UC3,cv::Scalar(0, 0, 0));
+        cv::Mat frame(previous_frame.rows, previous_frame.cols, CV_8UC3, cv::Scalar(0, 0, 0));
         interpolateFrame(forward_flow, previous_frame, frame, interpolation_value * i);
         interpolateFrame(backward_flow, current_frame, frame, 1 - (interpolation_value * i));
         result[i] = frame;
     }
+
+    cv::medianBlur(result, result, 3);
 
     return result;
 }
---
+++ src/VideoManager.h
@@ -0,0 +1,8 @@
+#ifndef LIBCAM_VIDEOMANAGER_H
+#define LIBCAM_VIDEOMANAGER_H
+
+class VideoManager {
+
+};
+
+#endif //LIBCAM_VIDEOMANAGER_H
